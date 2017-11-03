456438d5a80638c7f1619d67c993661e6731ed14
komsic <kayodemicheal159@gmail.com>
2017-11-03 20:00:30 +0100

    Move MainActivity to activity package

--- app/src/main/java/com/example/komsic/cryptoconverter/activity/MainActivity.java
+++ app/src/main/java/com/example/komsic/cryptoconverter/activity/MainActivity.java
@@ -1,4 +1,4 @@
-package com.example.komsic.cryptoconverter;
+package com.example.komsic.cryptoconverter.activity;
 
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
@@ -8,6 +8,8 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+
+import com.example.komsic.cryptoconverter.R;
 
 public class MainActivity extends AppCompatActivity {
 
