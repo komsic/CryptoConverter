9163baae44a657fe75ef69ea3cdf8c798c993412
komsic <kayodemicheal159@gmail.com>
2017-11-03 21:31:48 +0100

    Complete CurrencyConvertActivity layout

---
+++ app/src/main/java/com/example/komsic/cryptoconverter/activity/CurrencyConverterActivity.java
@@ -0,0 +1,20 @@
+package com.example.komsic.cryptoconverter.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+import com.example.komsic.cryptoconverter.R;
+
+public class CurrencyConverterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_currency_converter);
+    }
+
+    // TODO Remember to set conversion_result_layout visible
+    public void convert(View view) {
+    }
+}
